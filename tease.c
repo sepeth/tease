049f238ce903f520a72d5b594631ca6db58254f5
Doğan Çeçen <sepeth@gmail.com>
2023-07-22 19:21:15 +0300

    Reading from the child's output works

--- tease.c
+++ tease.c
@@ -4,17 +4,22 @@
 #include <stdlib.h> // exit
 #include <stdbool.h> // bool
 #include <stdarg.h> // va_start, va_end
+#include <sys/stat.h> // stat, fstat
+#include <sys/wait.h> // waitpid
+#include <time.h> // nanosleep
 #include <unistd.h> // mkstemp
 
 
+#define POLL_TIME_IN_MS 30
 #define FAILED_TO_WRITE_TO_STDERR 12
+#define HOW_MANY_BYTES_FROM_THE_END 200
 
 // Tenets/Self-guidance:
 //
 // - Try to make it non-platform-specific
-// - Don't dive into too specific before a working examples, e.g. fork, vfork, create_process etc.
+// - Don't dive into too specific before a working examples, e.g. fork, vfork, posix_spawnp etc.
 // - Don't worry about multiple paths yet; such as if enough memory, use mem instead of temp file.
-//   Just use a temp file for now.
+// - Just use a temp file for now.
 
 // List of things to watch out:
 // 
@@ -58,11 +63,47 @@ int main(int argc, char* argv[], char* envp[]) {
 		}
 	}
 
-	// SUCCESS
+	/*
+		How to read child's output and send to a file:
+			- Create a pipe
+			- Tie the pipe's input to the stdout of the child
+			- And read from the pie's output from the parent
+			- Then, we can grab the last line for print
+			- While writing the whole thing to the tempfile we created above.
+
+		Alternatively,
+			- Tie the pipe's output to the file directly
+			- Watch the file via inotify (or poll/read every 200ms).
+			- And read the changes
+
+		Decided to go with the alternative path and polling (inotify etc are platform
+		specific). And, I think piping from the child straight to the temp file would be
+		faster, though I didn't measure. Just assuming that in the first option, tease
+		process might become a bottleneck. With the approach I chose, we are not in between
+		that process, we just read from the output file at certain intervals.
+	*/
+
+	posix_spawn_file_actions_t file_actions;
+	if (posix_spawn_file_actions_init(&file_actions) < 0) {
+		perror("Couldn't init file actions");
+		goto cleanup_temp_file;
+	}
+
+	// addup2 closes the dest file descr (stdout) if it is open before duplication
+	if (posix_spawn_file_actions_adddup2(&file_actions, tmpfd, STDOUT_FILENO) < 0) {
+	 perror("Couldn't connect stdout to the temp file"); goto cleanup;
+	}
+
+	// What to do with the stderr
+	//  - Option 1: connect to the same output as stdout (will lose what is stdout and what is stderr) 
+	//  - Option 2: connect to another file (will lose the order)
+	//  - Option 3: don't touch it, let it go to the terminal (it will be printed before stdout if the process exit with non-zero, but better to let the user know about the errors and shell redirections would work).
+	// Let's go with the option 3. And, this is the easiest to implement (no code). Less is more once again.
+	pid_t child_pid;
 	int spawn_res = posix_spawnp(
-		/* pid */ NULL,
+		/* pid */ &child_pid,
 		/* file */ argv[1],
-		/* file actions */ NULL,
+		/* file actions */ &file_actions,
 		/* attrp */ NULL,
 		/* argv */ argv + 1,
 		envp
@@ -80,9 +121,90 @@ int main(int argc, char* argv[], char* envp[]) {
 		goto cleanup;
 	}
 
-	printf("tease still works\n");
+	// start polling the file
+	struct stat file_status;
+	struct timespec time_spec;
+	time_spec.tv_nsec = POLL_TIME_IN_MS * 1000 * 1000;
+	time_spec.tv_sec = 0;
+	int last_size = 0;
+	char last_line[HOW_MANY_BYTES_FROM_THE_END + 1];
+
+	// This is going to be useful to print last new line at the end.
+	bool printed_something = false;
+	while (true) {
+		// Let's wait a bit before we do anything
+		nanosleep(&time_spec, NULL);
+
+		if (fstat(tmpfd, &file_status) < 0) {
+			perror("Couldn't stat the temp file");
+		} else {
+			if (file_status.st_size > last_size) {
+				// There's stuff to read
+				if (lseek(tmpfd, -1 * HOW_MANY_BYTES_FROM_THE_END, SEEK_END) < 0) {
+					perror("Couldn't seek in the temp file");
+					continue;
+				}
+
+				int nread;
+				if ((nread = read(tmpfd, last_line, HOW_MANY_BYTES_FROM_THE_END)) < 0) {
+					perror("Couldn't read the temp file");
+					continue;
+				}
+
+				// Make it a C string
+				last_line[nread] = 0;
+
+				// If the child is doing buffered IO, there's a high chance that the last char is
+				// new line. Let's just ignore that
+				if (last_line[nread - 1] == '\n') {
+					last_line[nread - 1] = 0;
+				}
+				// Look for the \n character - this should work for windows too, since we care the rest, whether there's \r or not, we don't care
+				int pos_of_nl = nread - 1;
+				while (pos_of_nl > 0) {
+					if (last_line[--pos_of_nl] == '\n') {
+						break;
+					}
+				}
+
+				printf("\x1B[2K\r%s", last_line + pos_of_nl + 1);
+				fflush(stdout);
+				printed_something = true;
+
+				last_size = file_status.st_size;
+			}
+		}
+
+		int stat_loc;
+		// wait_res will be greater than zero (equals to child_pid) if the child is exited
+		// Hence we can break the loop
+		int wait_res = waitpid(child_pid, &stat_loc, WNOHANG);
+		if (wait_res < 0) {
+			perror("Failed to wait the child");
+			goto cleanup;
+		} else if (wait_res > 0) {
+			// Reflect the exit status of the child
+			exit_status = WEXITSTATUS(stat_loc);
+			if (WIFEXITED(stat_loc) && exit_status == 0) {
+				break; // SUCCESS
+			} else {
+				printf("Child failed\n");
+				// print the full content
+			}
+		}
+	}
+
+	// Write a newline at the end
+	if (printed_something) {
+		putchar('\n');
+	}
 
 cleanup:
+	if (posix_spawn_file_actions_destroy(&file_actions) < 0) {
+		perror("Couldn't destroy the file actions object");
+	}
+
+cleanup_temp_file:
 	if (delete_file_in_cwd) {
 		if (unlink(tmpfilename_in_cwd) < 0) {
 			perror("Deleting the temp file in current working dir has failed");
